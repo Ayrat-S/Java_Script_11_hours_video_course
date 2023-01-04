2f074f10f766bc232966dfef77bac6fa85641458
Ayrat <ayrat.jimbarez01@gmail.com>
2023-01-04 11:42:16 +0300

    Mod

---
+++ JS_training_files/Bogdan/3. 20.17 Самое важное в JavaScript/3.2 Практика.js
@@ -0,0 +1,15 @@
+
+/*  1. ОБЪЕКТ
+ОБЪЕКТ это НАБОР СВОЙСТВ (пар):
+"ИМЯ: ЗНАЧЕНИЕ" или "КЛЮЧ: ЗНАЧЕНИЕ".
+*/ 
+
+// 2. Пример объекта (находится между двумя фигурными скобками):
+let objectOne = {
+    visible: true,
+    colorDepth: 24,
+    title: 'My Image',
+    pixelDepth: 24,
+    width: 1440
+  }
+console.log(objectOne)
