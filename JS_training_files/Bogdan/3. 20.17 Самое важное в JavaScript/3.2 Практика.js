f291e0ef8b9875c14954e8fbab3c5b9b9c6db7ed
unknown <ayrat.jimbarez01@gmail.com>
2023-01-12 16:40:12 +0300

    Mod

--- JS_training_files/Bogdan/3. 20.17 Самое важное в JavaScript/3.2 Практика.js
+++ JS_training_files/Bogdan/3. 20.17 Самое важное в JavaScript/3.2 Практика.js
@@ -1,15 +1,17 @@
 
-/*  1. ОБЪЕКТ
-ОБЪЕКТ это НАБОР СВОЙСТВ (пар):
-"ИМЯ: ЗНАЧЕНИЕ" или "КЛЮЧ: ЗНАЧЕНИЕ".
-*/ 
-
-// 2. Пример объекта (находится между двумя фигурными скобками):
+// Так выглядит присвоение объекта для переменной:
 let objectOne = {
-    visible: true,
-    colorDepth: 24,
-    title: 'My Image',
-    pixelDepth: 24,
-    width: 1440
-  }
-console.log(objectOne)
+  visible: true,
+  colorDepth: 24,
+  title: 'My Image',
+  pixelDepth: 24,
+  width: 1440
+}
+
+// Практика:
+const aboutPerson = {
+  name: Ayrat,
+  age: 36,
+  height: 175,
+  weight: 80
+}
