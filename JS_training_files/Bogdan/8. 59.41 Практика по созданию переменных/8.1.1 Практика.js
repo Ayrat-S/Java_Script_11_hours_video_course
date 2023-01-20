32872c011c58529a43fc9e8e982d509caf511238
unknown <ayrat.jimbarez01@gmail.com>
2023-01-20 18:30:21 +0300

    Mod

---
+++ JS_training_files/Bogdan/8. 59.41 Практика по созданию переменных/8.1.1 Практика.js
@@ -0,0 +1,37 @@
+// **************************************************************************************************************************************************************************
+// Bogdan Stashchuk
+// https://www.youtube.com/watch?v=CxgOKJh4zWE
+
+// 59:41 Практика по созданию переменных
+
+// let myName
+
+// console.log(myName)
+
+// myName = 'Jim'
+
+// console.log(myName)
+
+// /*
+// >  console.log(myName)
+//    undefined / Действие, которое было выполнено внутри функции log / То что было выведено в консоль используя метод log
+// <. undefined / Результат вызова функции log / Также метод log вернул значение undefined - то что всегда возвращает метод log
+// */
+
+// console.log(console.log(10))
+
+// const myName // Missing initializer in const declaration
+
+// const myName = 'Jim'
+
+// myName = 'Jon' // Assignment to constant variable.
+
+// console.log(myName)
+
+
+
+
+
+
+
+
