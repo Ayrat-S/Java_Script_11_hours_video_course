7786dd2822143488654397a40689ef1fc3694595
unknown <ayrat.jimbarez01@gmail.com>
2023-10-13 15:47:38 +0300

    Mod

--- JS_training_files/Bogdan/3. 20.17 Самое важное в JavaScript/3.2 Практика.js
+++ JS_training_files/Bogdan/3. 20.17 Самое важное в JavaScript/3.2 Практика.js
@@ -5,18 +5,33 @@
 // ПРАКТИКА.
 
 // Так выглядит присвоение объекта для переменной:
+// let objectOne = {
+//   visible: true,
+//   colorDepth: 24,
+//   title: 'My Image',
+//   pixelDepth: 24,
+//   width: 1440
+// }
+
+// Ниже практикуемся самостоятельному написанию объектов:
+// const aboutPerson = {
+//   name: Ayrat,
+//   age: 36,
+//   height: 175,
+//   weight: 80
+// }
+
+// Пример объекта
 let objectOne = {
   visible: true,
   colorDepth: 24,
   title: 'My Image',
+  orintation: {
+    angle: 0,
+    type: 'landscape'
+  },
   pixelDepth: 24,
   width: 1440
 }
 
-// Ниже практикуемся самостоятельному написанию объектов:
-const aboutPerson = {
-  name: Ayrat,
-  age: 36,
-  height: 175,
-  weight: 80
-}
+console.table(objectOne)
