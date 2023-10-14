9d6bfc05f461f6870c70c5951045120aa8bd3724
unknown <ayrat.jimbarez01@gmail.com>
2023-10-14 14:34:47 +0300

    Mod

--- JS_training_files/Bogdan/00. Блокнот/Моя библиотека.js
+++ JS_training_files/Bogdan/00. Блокнот/Моя библиотека.js
@@ -1,13 +1,29 @@
 // Моя библиотека.
 
-// console.log(console.log('Name'))
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+/*
+console.log(console.log('Name')) // выведет в OUTPUT и сам текст 'Name' и его значение
+console.log('Name') // выведет в OUTPUT только сам текст 'Name'
+*/
+
+/* 
+console.log(console) // выведет в OUTPUT все свойства объекта console. 
+*/
+
+/*
+console.log(console.log) // выведет в OUTPUT значение свойства log.
+*/
+
+/*
+let Truck = {
+    Wheels: 8,
+    Windows: {
+        Front: 2,
+        Left: 1,
+        Right: 1
+    }
+}
+
+console.log(Truck.Windows.Front) // выведет в OUTPUT значение свойства вложенного объекта через точечную запись.
+*/
+
+console.table(console.log)
--- JS_training_files/Bogdan/00. Блокнот/Черновик.js
+++ JS_training_files/Bogdan/00. Блокнот/Черновик.js
@@ -1,7 +1,5 @@
 
-3 + 2
-
-'Name'
+console
 
 
 
