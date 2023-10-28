4e79a7cc15bed3ec2301b125cd5e2fa64bb0401e
unknown <ayrat.jimbarez01@gmail.com>
2023-10-28 16:48:35 +0300

    Mod

--- JS_training_files/Bogdan/00. Блокнот/Черновик.js
+++ JS_training_files/Bogdan/00. Блокнот/Черновик.js
@@ -1,3 +1,13 @@
-const myName = 'Name'
-myName = ''
-console.log(myName)
+const objectPerson1 = {
+    name: 'Jim',
+    age: 35
+}
+
+const objectPerson2 = objectPerson1
+console.log(objectPerson1)
+
+objectPerson2.age = 37
+console.log(objectPerson2)
+
+objectPerson1.car = true
+console.log(objectPerson2)
