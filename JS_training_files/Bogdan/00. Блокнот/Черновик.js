fb7f3590762d9fd434746dc989a1611a3c65bf88
unknown <ayrat.jimbarez01@gmail.com>
2023-12-09 14:48:07 +0300

    Mod

--- JS_training_files/Bogdan/00. Блокнот/Черновик.js
+++ JS_training_files/Bogdan/00. Блокнот/Черновик.js
@@ -1,19 +1,1 @@
-// function a() {
-//     console.log('Hey there')
-// }
-
-// a() // 'Hey there'
-
-// a = 10
-
-// a() // Uncaught TypeError: a is not a function
-
-const a = () => {
-    console.log('Hey there')
-}
-
-a()
-
-a = 10
-
 
