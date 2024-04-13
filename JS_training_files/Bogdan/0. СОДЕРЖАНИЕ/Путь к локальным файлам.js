5aa3f9a688e212d547f6b0125b3681e19dadf49e
unknown <ayrat.jimbarez01@gmail.com>
2024-04-13 14:38:31 +0300

    New file

---
+++ JS_training_files/Bogdan/0. СОДЕРЖАНИЕ/Путь к локальным файлам.js
@@ -0,0 +1,15 @@
+/* Где находятся все эти файлы?
+D:\TESTING\Vadim_Ksendzov\28_group\2_Java_Script\Репозиторий_JS_курс_11_часов\Java_Script_11_hours_video_course\JS_training_files\Bogdan
+*/
+
+/* Где находится локальный репозиторий JS?
+D:\TESTING\Vadim_Ksendzov\28_group\2_Java_Script\Репозиторий_JS_курс_11_часов\Java_Script_11_hours_video_course
+
+Как называется файл для внесения активности в обучении JS?:
+JavaScript - Полный Курс JavaScript Для Начинающих [11 ЧАСОВ]
+*/
+
+/* Ссылка на гуглдок по обучению "JavaScript - Полный Курс JavaScript Для Начинающих [11 ЧАСОВ]" (ayrat.jimbarez01@gmail.com):
+https://docs.google.com/spreadsheets/d/1NYTPZYfbZAWZ1G1v23wrSSjRZYZj3gMVIZnAgPSObuM/edit#gid=709428428
+*/
+
